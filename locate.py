c29bfcb8e8ba0f8929d4451ce37e3afe8bb077e6
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-11-09 14:11:29 -0400

    localized app.py commands

---
+++ locate.py
@@ -0,0 +1,8 @@
+# COMMANDS AND TEXTS
+ID_CHANNEL = 1038275898631274597
+WELCOME_REGISTER_CHANNEL = 'Welcome to joji server bot, here you will try the alpha version of joji bot I hope you like it and it can be distributed on any server! go to #information'
+HEI = '''Hello, I am a bot that allows you to listen to Joji's songs through a voice channel. go to j>embed'''
+JOIN_FAILED = 'You are not deep in a voice channel to execute this command'
+JOIN_OFF_TEXT = '''Actually... I'm not connected on a voice channel'''
+JOIN_OFF_PERFECT = 'I have been disconnected in the voice channel'
+ERROR_CHANNEL = 'You are not deep in a voice channel to execute this command'
